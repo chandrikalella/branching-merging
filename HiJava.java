d0175ae0dfb301034d7492b3e675e0f8fa1103a4
chandrikalella <lellachandrika123@gmail.com>
2021-11-11 18:54:39 +0530

    Added HiJava file

--- HiJava.java
+++ HiJava.java
@@ -2,7 +2,7 @@ public class HelloJava {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Hello Java branching-merging");
+		System.out.println("Hello Java - HiJava");
 		  
 		  
 	
